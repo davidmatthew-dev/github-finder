b0fe3f2d90370ccdb4a5d4eedbac3f166e047694
David Matthew <david@matthewenterprises.net>
2020-07-19 01:57:58 -0400

    Added Spinner

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner.js';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-	render() {
+const Users = ({ users, loading }) => {
+	if (loading) {
+		// Add spinner while data loads
+		return <Spinner />;
+	} else {
 		return (
 			<div style={userStyle}>
 				{/* now we are getting the users from Github's API */}
-				{this.props.users.map((user) => (
+				{users.map((user) => (
 					// each item should have a unique key property and we are also sending out the user property to be used in UserItem
 					<UserItem key={user.id} user={user} />
 				))}
 			</div>
 		);
 	}
-}
+};
+
+// define prop types
+Users.propTypes = {
+	users: PropTypes.array.isRequired,
+	loading: PropTypes.bool.isRequired,
+};
 
 // setup component styles
 const userStyle = {
