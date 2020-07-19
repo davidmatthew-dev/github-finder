0e955c4085810a70d2dda9409ea9c26c266d614d
David Matthew <david@matthewenterprises.net>
2020-07-18 23:31:07 -0400

    UserItem without constructor

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-	constructor() {
-		super();
-		this.state = {
-			id: 'id',
-			login: 'DCMatthew',
-			avatar_url: 'https://avatars0.githubusercontent.com/u/15695201?v=4',
-			html_url: 'https://github.com/DCMatthew',
-		};
-	}
+	state = {
+		id: 'id',
+		login: 'DCMatthew',
+		avatar_url: 'https://avatars0.githubusercontent.com/u/15695201?v=4',
+		html_url: 'https://github.com/DCMatthew',
+	};
 
 	render() {
+		//destructure this.state
+		const { login, avatar_url, html_url } = this.state;
 		return (
 			<div className="card text-center">
 				<img
-					src={this.state.avatar_url}
+					src={avatar_url}
 					className="round-img"
 					// inline styling
 					style={{ width: '60px' }}
 					alt="avatar"
 				/>
-				<h3>{this.state.login}</h3>
+				<h3>{login}</h3>
 				<div>
-					<a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+					<a href={html_url} className="btn btn-dark btn-sm my-1">
 						More
 					</a>
 				</div>
