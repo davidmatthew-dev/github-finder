5d4c7a1544667048fd36df7b55c4bce4b1caaa4c
David Matthew <david@matthewenterprises.net>
2020-07-18 22:25:38 -0400

    beginner info on react

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'; // instead of typing React.Component e
 import './App.css';
 
 class App extends Component {
+	fooMethod = () => "Bar - you're a method of the class";
 	// Class return so we use render to return data
 	render() {
+		// you can add javascript, functions or variables directly to the page
+		const name = 'ItzFeral';
+		const foo = () => 'Bar';
 		return (
+			// in jsx class are referred to as className instead of class
 			<div className="App">
-				<h1>Hello from React</h1>
+				{/* JSX must have one parent element */}
+				<h1>Hello, {name.toUpperCase()} from React</h1>
+				<h2>Oh, and Hi {foo()}, you're local!</h2>
+				<h2>I see you {this.fooMethod()} </h2>
 			</div>
 		);
 	}
