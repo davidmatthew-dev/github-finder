27e43ca0c4dfbaa3f36d5ada0639b0987944a213
David Matthew <david@matthewenterprises.net>
2020-07-18 21:45:43 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; // instead of typing React.Component each time, we also import Component from react
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	// Class return so we use render to return data
+	render() {
+		return (
+			<div className="App">
+				<h1>Hello from React</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
