87cdf5637ef832e9fbb327a71976a3ccdc623710
David Matthew <david@matthewenterprises.net>
2020-07-18 23:07:37 -0400

    add navbar component and imported fontawesome

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react'; // instead of typing React.Component each time, we also import Component from react
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
-	fooMethod = () => "Bar - you're a method of the class";
 	// Class return so we use render to return data
 	render() {
-		// you can add javascript, functions or variables directly to the page
-		const name = 'ItzFeral';
-		const foo = () => 'Bar';
 		return (
 			// in jsx class are referred to as className instead of class
 			<div className="App">
-				{/* JSX must have one parent element */}
-				<h1>Hello, {name.toUpperCase()} from React</h1>
-				<h2>Oh, and Hi {foo()}, you're local!</h2>
-				<h2>I see you {this.fooMethod()} </h2>
+				<Navbar />
+				<h1>Hello, from React</h1>
 			</div>
 		);
 	}
